fbdf7df6127ff5c56e17536126fec25331c281a4
sin <yuho.shin@gmail.com>
2022-06-02 18:25:32 +0900

    Initial Commit

---
+++ Divinglog2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Divinglog2
+//
+//  Created by 菊地英治 on 2022/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
